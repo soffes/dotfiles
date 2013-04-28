6fc769719ff338bb13082e0c238c8a1a20809528
Sam Soffes <sam@soff.es>
2013-04-28 16:12:11 -0400

    Only show git status not in the home folder

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -7,6 +7,9 @@ parse_git_dirty() {
 }
 
 function git_prompt_info() {
+  if [ $(git rev-parse --show-toplevel) '==' $HOME ]; then
+    return;
+  fi;
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
   echo " $(parse_git_dirty)${ref#refs/heads/}%{$reset_color%}"
 }

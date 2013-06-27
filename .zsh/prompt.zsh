8eba45e6da84f28670acb8e41de53cfd8850892a
Sam Soffes <sam@soff.es>
2013-06-27 12:15:58 -0400

    Improve git prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -7,9 +7,17 @@ parse_git_dirty() {
 }
 
 function git_prompt_info() {
-  if [ $(git rev-parse --show-toplevel) '==' $HOME ]; then
+  # Ignore directorys that aren't in git
+  if ! git ls-files >& /dev/null; then
     return;
   fi;
+
+  # Ignore the home directory
+  if [ $(git rev-parse --quiet --show-toplevel) '==' $HOME ]; then
+    return;
+  fi;
+
+  # Get the branch name and color
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
   echo " $(parse_git_dirty)${ref#refs/heads/}%{$reset_color%}"
 }

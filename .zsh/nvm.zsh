79016937206333144d1cd2d30941315305061fb0
Sam Soffes <sam@soff.es>
2016-06-29 07:07:48 -0700

    Add NVM config

---
+++ .zsh/nvm.zsh
@@ -0,0 +1,7 @@
+export NVM_DIR="$HOME/.nvm"
+
+# Loading NVM greatly reduces tab open time
+# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+# Alias to load NVM on demand since I don't use it that often.
+alias loadnvm="source $NVM_DIR/nvm.sh"

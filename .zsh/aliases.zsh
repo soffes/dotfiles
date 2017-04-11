e407754e2820fb09d7fe74c67a940c9d36e65086
Sam Soffes <sam@soff.es>
2017-04-11 12:47:37 -0400

    Update brewup
    https://twitter.com/juanchopx2/status/851838513129697280

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -33,4 +33,4 @@ alias %=' '
 alias $=' '
 
 # Homebrew
-alias brewup='brew update && brew doctor && brew outdated && brew upgrade && brew cleanup'
+alias brewup='brew update && brew doctor && brew outdated && brew upgrade && brew cleanup -s --prune=1'

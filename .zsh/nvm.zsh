8ceb104b241e0c8da3f62f0e3e493d3fe4554fb1
Sam Soffes <sam@soff.es>
2017-10-23 13:41:06 -0700

    Remove NVM config and add brew Python to path

--- .zsh/nvm.zsh
+++
@@ -1,7 +0,0 @@
-export NVM_DIR="$HOME/.nvm"
-
-# Loading NVM greatly reduces tab open time
-# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-
-# Alias to load NVM on demand since I don't use it that often.
-alias loadnvm="source $NVM_DIR/nvm.sh"

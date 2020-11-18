5b3d6563adf7b7437153c59ef23a8fc4decf162a
Sam Soffes <sam@soff.es>
2020-11-17 19:19:49 -0800

    Support Homebrew on ARM

--- .zsh/rbenv.zsh
+++ .zsh/rbenv.zsh
@@ -1,3 +1,3 @@
 autoload -U ~/.rbenv/shims
 export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH"
-export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=/opt/homebrew/Cellar/openssl@1.1/1.1.1h"

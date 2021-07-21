44b64bd8d755711a15b123e8d05051e44e9c5c1a
Sam Soffes <sam@soff.es>
2021-07-21 16:33:58 -0700

    Update OpenSSL constant

--- .zsh/rbenv.zsh
+++ .zsh/rbenv.zsh
@@ -1,3 +1,3 @@
 autoload -U ~/.rbenv/shims
 export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH"
-export RUBY_CONFIGURE_OPTS="--with-openssl-dir=/opt/homebrew/Cellar/openssl@1.1/1.1.1h"
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=/opt/homebrew/Cellar/openssl@1.1/1.1.1k"

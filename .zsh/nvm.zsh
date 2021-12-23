06fe4673e4d11d2ded76029f45a5da6874d576a7
Sam Soffes <sam@soff.es>
2021-12-23 07:42:40 -0800

    Update nvm config

--- .zsh/nvm.zsh
+++ .zsh/nvm.zsh
@@ -2,11 +2,8 @@ export NVM_DIR="$HOME/.nvm"
 
 # Add node to path
 # Inspired by https://www.ioannispoulakas.com/2020/02/22/how-to-speed-up-shell-load-while-using-nvm/
-export PATH="$NVM_DIR/versions/node/v16.13.0/bin:$PATH"
-
-# Load nvm
-#[[ -s "$NVM_DIR/nvm.sh" ]] && source "$NVM_DIR/nvm.sh" --no-use
+export PATH="$NVM_DIR/versions/node/v17.0.1/bin:$PATH"
 
 # Standard integration
-#[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && . "/opt/homebrew/opt/nvm/nvm.sh"
-#[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && . "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+# [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
+# [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"

6979ccc6dbe9b1b0b356fa259a83d3a3c96d8695
Sam Soffes <sam@soff.es>
2022-05-29 19:44:24 -0700

    Update node

--- .zsh/nvm.zsh
+++ .zsh/nvm.zsh
@@ -2,7 +2,7 @@ export NVM_DIR="$HOME/.nvm"
 
 # Add node to path
 # Inspired by https://www.ioannispoulakas.com/2020/02/22/how-to-speed-up-shell-load-while-using-nvm/
-export PATH="$NVM_DIR/versions/node/v17.0.1/bin:$PATH"
+export PATH="$NVM_DIR/versions/node/v18.1.0/bin:$PATH"
 
 # Standard integration
 # [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"

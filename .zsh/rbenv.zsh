62cdc4d99e043ba60643fc0f16264bdce1fbea2b
Sam Soffes <sam@soff.es>
2024-11-07 11:56:20 -0800

    Use openssl@3 for ruby-build

--- .zsh/rbenv.zsh
+++ .zsh/rbenv.zsh
@@ -2,4 +2,4 @@ autoload -U ~/.rbenv/shims
 export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH"
 
 # Hardcoding instead of asking Homebrew greatly speeds up starting a new terminal session
-export RUBY_CONFIGURE_OPTS="--with-openssl-dir=/opt/homebrew/opt/openssl@1.1"
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=/opt/homebrew/opt/openssl@3"

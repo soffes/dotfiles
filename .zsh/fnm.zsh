ad2e05b23abb0898e3bb639017f5dcd7b38d7b87
Sam Soffes <sam@soff.es>
2025-11-14 05:15:15 -0800

    fnm config

---
+++ .zsh/fnm.zsh
@@ -0,0 +1,10 @@
+export PATH="${HOME}/.local/state/fnm_multishells/33033_1763125848239/bin":$PATH
+export FNM_MULTISHELL_PATH="${HOME}/.local/state/fnm_multishells/33033_1763125848239"
+export FNM_VERSION_FILE_STRATEGY="local"
+export FNM_DIR="${HOME}/.local/share/fnm"
+export FNM_LOGLEVEL="error"
+export FNM_NODE_DIST_MIRROR="https://nodejs.org/dist"
+export FNM_COREPACK_ENABLED="false"
+export FNM_RESOLVE_ENGINES="true"
+export FNM_ARCH="arm64"
+rehash
